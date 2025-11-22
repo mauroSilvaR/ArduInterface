647568daf3cdbbc9b89f3b66530bff811143ed23
mauroSilvaR <msr_hck1@hotmail.com>
2025-11-22 18:06:13 -0300

    added grid and reset button

--- GUI.py
+++ GUI.py
@@ -1,31 +1,53 @@
 
 import tkinter as tk
 
-def on_button_click(i):
-    # Update label when button is clicked
-    status_label.config(text=f"You clicked{i}")
+GRID_ROWS = 8
+GRID_COLUMNS = 8
 
-# Create main Window
+# List to hold the buttons
+buttons = []
+
+def on_button_click(i, j):
+    btn = buttons[j][i]
+    current_color = btn.cget("bg")
+    new_color = "red" if current_color == "white" else "white"
+    btn.config(bg=new_color)
+
+def reset_grid():
+    for i in range(0,GRID_ROWS):
+        for j in range (0,GRID_COLUMNS):
+            btn = buttons[j][i]
+            new_color = "white"
+            btn.config(bg=new_color)
+            status_label.config(text=f"All buttons reset!")
+
+# Main window
 root = tk.Tk()
 root.title("ArduInterface")
 
-# Frame for buttons, maybe change to grid later
+# Frame for buttons
 frame_buttons = tk.Frame(root)
 frame_buttons.pack(pady=10)
 
-# button creation, after mvp can be better, for now, use for
-for i in range(1, 8): 
-    btn = tk.Button(frame_buttons, text=f"LED {i}", width=10,
-                    command=lambda i=i: on_button_click(i))
-    btn.pack(side="left", padx=5)
+# Creating the grid of buttons
+for j in range(GRID_ROWS):
+    row_buttons = []
+    for i in range(GRID_COLUMNS):
+        btn = tk.Button(frame_buttons, bg="white",
+                        command=lambda i=i, j=j: on_button_click(i, j))
+        btn.grid(row=j, column=i, padx=2, pady=2)
+        row_buttons.append(btn)
+    buttons.append(row_buttons)
 
-# Frame for message
+# Frame for messages
 frame_status = tk.Frame(root)
 frame_status.pack(pady=10)
 
-status_label = tk.Label(frame_status, text="Click on the buttons")
-status_label.pack()
+reset_button = tk.Button(frame_status,command=reset_grid)
+reset_button.grid(row=0,column=0)
+status_label = tk.Label(frame_status, text="Click on the buttons to change their state")
+status_label.grid(row=0,column=1)
 
-# Inicia o loop da interface
+
+
 root.mainloop()
-
